b15eafc6dc60e22f45eb7f12ec1045afe5db864a
Alina <lindenalina.al@gmail.com>
2020-07-03 16:34:58 +0100

    film CRUD passed

--- console.rb
+++ console.rb
@@ -5,6 +5,7 @@ require_relative('models/ticket')
 require('pry-byebug')
 
 Customer.delete_all()
+Film.delete_all()
 
 customer1 = Customer.new({'name' => 'Ben', 'funds' => 50})
 customer2 = Customer.new({'name' => 'Alina', 'funds' => 35})
@@ -12,8 +13,15 @@ customer2 = Customer.new({'name' => 'Alina', 'funds' => 35})
 customer1.save()
 customer2.save()
 
+customer1.name = "Benjamin"
+customer1.update()
 
+film1 = Film.new({'title' => 'Arrival', 'price' => 7})
 
+film1.save()
+
+film1.price = 8
+film1.update()
 
 binding.pry
 nil
--- models/film.rb
+++ models/film.rb
@@ -1,4 +1,4 @@
-require_relative('../db/film')
+require_relative('../db/sql_runner')
 
 class Film
 
@@ -11,6 +11,34 @@ class Film
         @price = options['price'].to_i
     end
 
-    
+    def save()
+        sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
+        values = [@title, @price]
+        film_data = SqlRunner.run(sql, values).first
+        @id = film_data['id'].to_i
+    end
+
+    def self.delete_all()
+        sql = "DELETE FROM films"
+        SqlRunner.run(sql)
+    end
+
+    def delete()
+        sql = "DELETE FROM films where id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
+    def update()
+        sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+        values = [@title, @price, @id]
+        SqlRunner.run(sql, values)
+    end
+
+    def self.all()
+        sql = "SELECT * FROM films"
+        returned_films = SqlRunner.run(sql)
+        return returned_films.map { |film| Film.new(film) }
+    end
 
 end
